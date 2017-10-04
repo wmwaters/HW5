b360be53a96b275f7a5bc3bb39b5b8011633b3b3
William Waters <wmwaters@umich.edu>
2017-10-04 18:49:19 -0400

    finish hw5

--- hw5.py
+++ hw5.py
@@ -1,9 +1,8 @@
 import re
-file = open('regex_sum_32323').read()
+file = open('regex_sum_32323.txt').read()
+small_list = []
 num_list = []
-for line in file:
-	small_list = []
-	small_list = re.findall([0-9]+)
-	for item in small_list:
-		num_list.append(int(item))
+small_list = re.findall('([0-9]+)',file)
+for item in small_list:
+	num_list.append(int(item))
 print(sum(num_list))
